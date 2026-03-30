599767b667f06011e09c925c3a8dd32d969c64be
Gharshith Reddy R <126974070+zuckyhacker@users.noreply.github.com>
2026-03-30 16:42:36 +0530

    Update model.py

---
+++ model.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import LabelEncoder
+import pickle
+
+data = pd.read_csv("mobile_data.csv")
+
+if 'price_range' in data.columns:
+    data['price'] = data['price_range'] * 10000
+
+le = LabelEncoder()
+data['brand'] = le.fit_transform(data['brand'])
+
+X = data.drop(['price', 'price_range'], axis=1, errors='ignore')
+y = data['price']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = RandomForestRegressor(n_estimators=200)
+model.fit(X_train, y_train)
+
+print("Accuracy:", model.score(X_test, y_test))
+
+pickle.dump(model, open("model.pkl", "wb"))
+pickle.dump(X.columns, open("features.pkl", "wb"))
+pickle.dump(le, open("brand_encoder.pkl", "wb"))
